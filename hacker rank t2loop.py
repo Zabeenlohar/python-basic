8960abc6055d683f41d6af18e726784e58ff247a
Zabeenlohar <113299193+Zabeenlohar@users.noreply.github.com>
2024-04-27 09:19:54 +0530

    Create hacker rank t2loop.py

---
+++ hacker rank t2loop.py
@@ -0,0 +1,11 @@
+#The included code stub will read an integer n from studing without using any string method,try to print the following 123....n
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
+
+
+#Input (stdin)
+3
+Your Output (stdout)
+123
